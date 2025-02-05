ea4af6843195832e8755af576a2791f350924d21
Se7enMuting <se7en@muting.cc>
2025-02-05 20:30:56 +0800

    add Version

--- BTC/BIP39/Tools/clipboard/script.js
+++ BTC/BIP39/Tools/clipboard/script.js
@@ -1,16 +1,14 @@
+// <!-- Version: v0.0.1 | GitHub: @Se7enMuting -->
 document.getElementById('pasteArea').addEventListener('paste', function(e) {
-    // 阻止默认行为
     e.preventDefault();
-    // 获取粘贴板数据
     var items = (e.clipboardData || e.originalEvent.clipboardData).items;
-    console.log(JSON.stringify(items)); // 可选：控制台打印所有粘贴板数据
+    console.log(JSON.stringify(items));
     for (index in items) {
         var item = items[index];
         if (item.kind === 'file') {
             var blob = item.getAsFile();
             var reader = new FileReader();
             reader.onload = function(event){
-                // 显示图片
                 document.getElementById('pastedImage').src = event.target.result;
             }; 
             reader.readAsDataURL(blob);
